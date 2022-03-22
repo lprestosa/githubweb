8939586a1c6c0dae27b03c6ce6ccee265577266e
zparman <lprestosa@yahoo.com>
2022-03-22 12:45:24 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#
+"""
+Code exampl
+"""
+
+
+project_name = 'githubweb'
+project_desc = """
+Free website hosted by Github.com
+Article : how-to-build-your-free-website-in-one-day
+     Url: https://blog.devgenius.io/how-to-build-your-free-website-in-one-day-dc1bd4e91bf9
+"""
+
+def main():
+   print(f'Project: {project_name}')
+   print(project_desc)
+
+
+if __name__ == '__main__':
+    main()
+
+
